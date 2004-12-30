46977efa7460477884e8c65534d140cef115f8f2
ttroxell <ttroxell@a08c9ffe-0e18-0410-9f84-b740a85aaf4c>
2004-12-30 06:46:38 +0000

    revert cron fix, policy violation

--- debian/logcheck.cron.d
+++ debian/logcheck.cron.d
@@ -3,7 +3,7 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 MAILTO=root
 
-##ENABLED_IN_POSTINST## @reboot         logcheck    if [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck -R; fi
-##ENABLED_IN_POSTINST## 2 * * * *       logcheck    if [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck; fi
+@reboot         logcheck    if [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck -R; fi
+2 * * * *       logcheck    if [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck; fi
 
 # EOF
